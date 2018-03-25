f0bbcfc0c1d416467512a06dc6fd27374c1b5894
vladkanash <vlad.kanash@gmail.com>
2018-03-25 20:00:03 +0300

    added text inputs.

--- app/index.js
+++ app/index.js
@@ -5,6 +5,9 @@ import Home from './screens/Home';
 EStyleSheet.build({
   $primaryBlue: '#4F6D7A',
   $white: '#fff',
+  $border: '#E2E2E2',
+  $input: '#797979',
+  $lightGrey: '#888888',
 });
 
 export default () => <Home />;
