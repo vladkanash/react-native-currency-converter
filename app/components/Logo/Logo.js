b3c806abaddd9151634c9c18fe3d8b79d1a9d5fc
vladkanash <vlad.kanash@gmail.com>
2018-03-25 19:03:37 +0300

    added logo.

---
+++ app/components/Logo/Logo.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Image, Text } from 'react-native';
+
+import styles from './styles';
+
+const Logo = () => (
+  <View style={styles.container}>
+    <Image style={styles.containerImage} source={require('./images/logo.png')} />
+    <Text style={styles.text}>Currency Converter</Text>
+  </View>
+);
+
+export default Logo;
