98ec9b8cf5b719dcde97db05c75677ac79dcd053
vladkanash <vlad.kanash@gmail.com>
2018-03-28 23:16:24 +0300

    text info under input.

--- app/screens/Home.js
+++ app/screens/Home.js
@@ -5,11 +5,14 @@ import { Container } from '../components/Container';
 import { Logo } from '../components/Logo';
 import { InputWithButton } from '../components/TextInput';
 import { ClearButton } from '../components/Buttons';
+import { LastConverted } from '../components/Text';
 
 const TEMP_BASE_CURRENCY = 'USD';
 const TEMP_QUOTE_CURRENCY = 'GBP';
 const TEMP_BASE_PRICE = '100';
 const TEMP_QUOTE_PRICE = '79.49';
+const TEMP_CONVERSION_RATE = 0.7974;
+const TEMP_CONVERSION_DATE = new Date();
 
 class Home extends Component {
     handlePressBaseCurrency = () => {
@@ -47,6 +50,12 @@ class Home extends Component {
             defaultValue={TEMP_QUOTE_PRICE}
             keyboardType="numeric"
             onChangeText={this.handleTextChange}
+          />
+          <LastConverted
+            base={TEMP_BASE_CURRENCY}
+            quote={TEMP_QUOTE_CURRENCY}
+            date={TEMP_CONVERSION_DATE}
+            conversionRate={TEMP_CONVERSION_RATE}
           />
           <ClearButton
             text="Reverse Currencies"
